142a1ab38c3b719787bae3906778549cc02131fe
Julio Muñoz Gallarin <pretonik1978@yahoo.es>
2015-01-19 23:04:59 +0100

    hystrix server

---
+++ HeystrixDashboard/src/main/java/testingtools/HeystrixDashboard/HeystrixDashboardApp.java
@@ -0,0 +1,21 @@
+package testingtools.HeystrixDashboard;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableHystrixDashboard
+@ComponentScan
+@EnableTurbine
+public class HeystrixDashboardApp 
+{
+    public static void main( String[] args )
+    {
+    	new SpringApplicationBuilder(HeystrixDashboardApp.class).web(true).run(args);
+    }
+}
